f37310170a4311056a4fa7c087ef3ce2a7b3fb01
YL H <1074747648@qq.com>
2024-03-06 14:42:48 +0800

    前端临时

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,38 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>后端的数据：{{ x }}</div>
+  <NavBar/>
   <router-view/>
 </template>
 
+<script>
+import $ from 'jquery'
+import {ref} from 'vue'
+
+import NavBar from '@/components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+
+export default {
+  name:"app",
+  setup:() => {
+    let x = ref("");
+    $.ajax({
+      url:"http://127.0.0.1:3000/pk/getbotinfo/",
+      type:"get",
+      success:resp=>{
+        console.log(resp);
+        x.value=resp;
+        console.log(x.value);
+      }
+    });
+    return {x};
+  },
+  components:{
+    NavBar
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
