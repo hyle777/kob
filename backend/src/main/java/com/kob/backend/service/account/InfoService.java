978f0c13eff3e5ef42ec073aa90a00322c061e96
YL H <1074747648@qq.com>
2024-03-08 19:04:13 +0800

    修改目录结构

--- backend/src/main/java/com/kob/backend/service/account/InfoService.java
+++
@@ -1,7 +0,0 @@
-package com.kob.backend.service.account;
-
-import java.util.Map;
-
-public interface InfoService {
-    public Map<String,String> getInfo();
-}
--- backend/src/main/java/com/kob/backend/service/account/RegisterService.java
+++
@@ -1,7 +0,0 @@
-package com.kob.backend.service.account;
-
-import java.util.Map;
-
-public interface RegisterService {
-    public Map<String,String> register(String username, String password,String confirmedPassword);
-}
